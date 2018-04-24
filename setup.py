4cddc81b500994eec445c160df12e73a7d14c141
Ben Thompson <t.ben.thompson@gmail.com>
2018-04-24 15:34:57 -0400

    18.04.24

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 version = open('VERSION').read()
 
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = open('README.md').read()
+
 setup(
     packages = ['anti_forgetful'],
 
@@ -17,4 +23,11 @@ setup(
     name = 'anti_forgetful',
     version = version,
     description = '',
+    long_description = description,
+
+    url = 'https://github.com/tbenthompson/anti_forgetful',
+    author = 'T. Ben Thompson',
+    author_email = 't.ben.thompson@gmail.com',
+    license = 'MIT',
+    platforms = ['any']
 )
